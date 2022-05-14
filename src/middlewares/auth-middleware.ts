34ed1ad16780903b68b234b442a63176093ac9af
Karter13 <KarMiH1975@mail.ru>
2022-05-14 11:30:45 +0300

    fix middleware 3

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -3,11 +3,14 @@ import {NextFunction, Request, Response} from "express";
 const badIdArr = ["191.168.1"]
 
 export const checkHeadersMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const expectedValue =  "YWRtaW46cXdlcnR5";
+    const exceptedAuthorizationValue = "Basic YWRtaW46cXdlcnR5"
     const autorised = req.headers?.authorization?.split(' ')[1];
-    if (autorised === expectedValue) {
-        next()
-    } else {
+
+    if(!req.headers || !req.headers.authorization ) {
         res.sendStatus(401)
-    };
+    } else if(req.headers.authorization !== exceptedAuthorizationValue) {
+        res.sendStatus(401)
+    } else {
+        next()
+    }
 }
